3d0bd503879f2ff7c956637536275e33ec37a68b
xupeng <412984223@qq.com>
2018-01-12 17:52:13 +0800

    增加feature

---
+++ src/main/scala/com/za/apps/feature/OneHotEncoder.scala
@@ -0,0 +1,33 @@
+package com.za.apps.feature
+
+import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
+import org.apache.spark.sql.SparkSession
+
+object OneHotEncoder {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "D:\\服务器管理\\平台软件\\hadoop-2.6.4\\hadoop-2.6.4")
+    val spark = SparkSession.builder().appName("OneHotEncoder").master("local[*]").getOrCreate()
+    import spark.implicits._
+    val df = spark.createDataFrame(Seq(
+      (0, "a"),
+      (1, "b"),
+      (2, "c"),
+      (3, "a"),
+      (4, "a"),
+      (5, "c")
+    )).toDF("id", "category")
+
+    val indexer = new StringIndexer()
+      .setInputCol("category")
+      .setOutputCol("categoryIndex")
+      .fit(df)
+    val indexed = indexer.transform(df)
+
+    val encoder = new OneHotEncoder()
+      .setInputCol("categoryIndex")
+      .setOutputCol("categoryVec")
+
+    val encoded = encoder.transform(indexed)
+    encoded.show()
+  }
+}
